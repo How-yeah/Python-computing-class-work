ab5fb3a6d59e87ba6e155b2bf7132fdd1445e933
Trouvaille0198 <1375911011@qq.com>
2021-04-01 19:12:59 +0800

    change example

--- experiment1/count_words.py
+++ experiment1/count_words.py
@@ -1,7 +1,8 @@
 from word_counter import WordCounter
 
-key_words = ['and', 'so', 'if', 'the', 'all', 'but', 'while', 'is']
-Counter = WordCounter(key_words)
-# frequency = Counter.count_words()
-frequency = Counter.count_key_words()
+
+Counter = WordCounter()
+frequency = Counter.count_words()
+# key_words = ['so', 'if', 'the', 'all', 'but', 'and', 'while', 'is', 'happy']
+# frequency = Counter.count_key_words()
 Counter.save(frequency)
