02c209b708e4874c82c6ddc400579a541864a8b3
LJW <2505214118@qq.com>
2021-04-08 09:56:37 +0800

    add experiment1

---
+++ experiment1/count.py
@@ -0,0 +1,8 @@
+a_list=[]
+with open('../english.txt', encoding='utf-8') as f:
+	str1=f.read()
+a_list=str1.split(' ')
+print(a_list)
+from collections import Counter 
+result = Counter(a_list) 
+print(result)
---
+++ experiment1/count1.py
@@ -0,0 +1,13 @@
+a_list = []
+count = 0
+with open('../english.txt', encoding='utf-8') as f:
+    str1 = f.read()
+a_list = str1.split(' ')
+b_list = ['abc', 'rewq', 'lkj']
+for i in b_list:
+    for j in a_list:
+        if i == j:
+            count = count + 1
+    print(i,':',count)
+    count = 0
+         
