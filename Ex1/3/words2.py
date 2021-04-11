d6c72b445ba6dd9d8b0c86648fd566a33726fe04
LJW0316 <lijingwei010316@163.com>
2021-04-11 10:11:53 +0800

    改进了反序对的查找算法

--- Ex1/3/words2.py
+++ Ex1/3/words2.py
@@ -1,19 +1,20 @@
 from time import time
+
 start = time()
 readFile = open('words.txt', 'r')
 worldList = readFile.readlines()  # 将文件内单词存在列表中
+wordDict = dict(zip(worldList, worldList))  # 将单词存入字典中
 readFile.close()
 res = []
 for word1 in worldList:
     word = word1[-2::-1]  # 获得原单词去掉回车符的逆序单词
-    for word2 in worldList:  # 在列表中查找是否存在逆序单词
-        if word == word2[:-1]:
-            res.append((word1[:-1], word2[:-1]))
-            print((word1[:-1], word2[:-1]))  # 输出结果
-            break
+    word += '\n'
+    if word in wordDict:  # 在字典中查找是否存在逆序单词
+        res.append((word1[:-1], word[:-1]))
+        print((word1[:-1], word[:-1]))  # 输出结果
 end = time()
 # 将结果输出到文件中.
-writeFile = open('result.txt', 'w')
+writeFile = open('result2.txt', 'w')
 for x in res:
     print(x, file=writeFile)
 print(end - start, file=writeFile)
