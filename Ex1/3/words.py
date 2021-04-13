8f741fc78c7c987975c7b7ec2b83496df9eff7c4
LJW0316 <lijingwei010316@163.com>
2021-04-13 17:21:18 +0800

    优化了逆序对查找算法

--- Ex1/3/words.py
+++ Ex1/3/words.py
@@ -8,9 +8,10 @@ for word1 in worldList:
     word = word1[-2::-1]  # 获得原单词去掉回车符的逆序单词
     for word2 in worldList:  # 在列表中查找是否存在逆序单词
         if word == word2[:-1]:
-            res.append((word1[:-1], word2[:-1]))
-            print((word1[:-1], word2[:-1]))  # 输出结果
-            break
+            if word1 != word2 and (word2[:-1], word1[:-1]) not in res:  # 去除本身是回文串，以及重复的逆序对
+                res.append((word1[:-1], word2[:-1]))
+                print((word1[:-1], word2[:-1]))  # 输出结果
+                break
 end = time()
 # 将结果输出到文件中.
 writeFile = open('result.txt', 'w')
--- Ex1/3/words2.py
+++ Ex1/3/words2.py
@@ -10,8 +10,9 @@ for word1 in worldList:
     word = word1[-2::-1]  # 获得原单词去掉回车符的逆序单词
     word += '\n'
     if word in wordDict:  # 在字典中查找是否存在逆序单词
-        res.append((word1[:-1], word[:-1]))
-        print((word1[:-1], word[:-1]))  # 输出结果
+        if word != word1 and ((word[:-1], word1[:-1]) not in res):  # 去除本身是回文串，以及重复的逆序对
+            res.append((word1[:-1], word[:-1]))
+            print((word1[:-1], word[:-1]))  # 输出结果
 end = time()
 # 将结果输出到文件中.
 writeFile = open('result2.txt', 'w')
