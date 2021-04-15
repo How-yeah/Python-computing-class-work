23ec165f95a03cb291c7a1ac3ddb96955f9f78d8
LJW <2505214118@qq.com>
2021-04-15 10:55:26 +0800

    little changes

--- experiment1/count1.py
+++ experiment1/count1.py
@@ -4,10 +4,10 @@ with open(r'./experiment1/data/english.txt') as f:
     str1 = f.read()
 a_list = str1.split(' ')
 b_list = ['abc', 'rewq', 'lkj']
+words_dict = dict(zip(a_list,a_list))
 for i in b_list:
-    for j in a_list:
-        if i == j:
-            count = count + 1
+    if i in words_dict:
+        count = count + 1
     print(i,':',count)
     count = 0
          
